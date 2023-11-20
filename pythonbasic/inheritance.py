3ddb0619ed91f2c85e4cdd02f1ae7dc30c6eac80
Shivangi varshney <132803861+Shivangii13@users.noreply.github.com>
2023-11-20 22:31:30 +0530

    commit initial

---
+++ pythonbasic/inheritance.py
@@ -0,0 +1,17 @@
+class Employee:
+    def __init__(self , name , id) -> None:
+        self.name = name
+        self.id = id
+
+    def showDetails(self):
+        print(f"the name of employee:{self.id} is {self.name}")
+
+   # class programmer(employee):
+        #def showLanguage(self):
+           # print("the default language is python")
+
+    e1 = Employee("Rohan Das",890)
+    e1.showDetails()
+    e2 = Employee("Rohan Das",890)
+    e2.showDetails()
+    
